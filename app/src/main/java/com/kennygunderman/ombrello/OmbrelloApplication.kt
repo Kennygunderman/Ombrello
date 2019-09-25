82a8e05fa8606d7a81e4ebe989993957a3bf52e7
Kenny <kennygunderman@gmail.com>
2019-09-25 17:31:25 -0400

    Add date util to app module

--- app/src/main/java/com/kennygunderman/ombrello/OmbrelloApplication.kt
+++ app/src/main/java/com/kennygunderman/ombrello/OmbrelloApplication.kt
@@ -3,6 +3,7 @@ package com.kennygunderman.ombrello
 import android.app.Application
 import com.kennygunderman.ombrello.di.apiModule
 import com.kennygunderman.ombrello.di.locationServiceModule
+import com.kennygunderman.ombrello.di.utilModule
 import com.kennygunderman.ombrello.di.vmModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -13,7 +14,8 @@ class OmbrelloApplication: Application() {
     private val appModules = listOf(
         vmModule,
         apiModule,
-        locationServiceModule
+        locationServiceModule,
+        utilModule
     )
 
     override fun onCreate() {
